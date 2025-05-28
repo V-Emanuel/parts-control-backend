22575fc48e3378b59c0c87dcb20c74a39caa0348
V-Emanuel <gvictor567@gmail.com>
2025-05-28 06:53:24 -0300

    deploy

--- database/seeders/company_seeder.ts
+++ database/seeders/company_seeder.ts
@@ -4,10 +4,7 @@ import Company from '#models/company'
 export default class extends BaseSeeder {
   async run() {
     await Company.createMany([
-      { name: 'Empresa A', code: 101 },
-      { name: 'Empresa B', code: 202 },
-      { name: 'Empresa C', code: 303 },
-      { name: 'Empresa D', code: 404 },
+      { name: 'Peugeot', code: 41 },
     ])
   }
 }
