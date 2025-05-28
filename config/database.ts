66f5e9c493ce615efa0ccf4b5cf91d3818eab3dd
V-Emanuel <gvictor567@gmail.com>
2025-05-28 05:20:39 -0300

    config: update db to postgres

--- config/database.ts
+++ config/database.ts
@@ -2,21 +2,18 @@ import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'mysql',
+  connection: 'pg',
   connections: {
-    mysql: {
-      client: 'mysql2',
+    pg: {
+      client: 'pg',
       connection: {
-        host: env.get('DB_HOST'),
-        port: env.get('DB_PORT'),
-        user: env.get('DB_USER'),
-        password: env.get('DB_PASSWORD'),
-        database: env.get('DB_DATABASE'),
+        host: env.get('PG_HOST'),
+        port: Number(env.get('PG_PORT')),
+        user: env.get('PG_USER'),
+        password: env.get('PG_PASSWORD'),
+        database: env.get('PG_DB_NAME'),
       },
-      migrations: {
-        naturalSort: true,
-        paths: ['database/migrations'],
-      },
+      debug: false,
     },
   },
 })
