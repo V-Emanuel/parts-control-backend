dabf036f5897b6d26b1073695d91fb6084ddadde
V-Emanuel <gvictor567@gmail.com>
2025-05-31 12:01:47 -0300

    config: dockerfile

--- database/seeders/company_seeder.ts
+++ database/seeders/company_seeder.ts
@@ -5,6 +5,9 @@ export default class extends BaseSeeder {
   async run() {
     await Company.createMany([
       { name: 'Peugeot', code: 41 },
+      { name: 'emp 1', code: 41 },
+      { name: 'emp 2', code: 41 },
+      { name: 'emp 3', code: 41 },
     ])
   }
 }
--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -11,76 +11,76 @@ export default class extends BaseSeeder {
         admin: true,
         active: true,
       },
-      // {
-      //   fullName: 'admin 2', //2
-      //   email: 'victor@example.com',
-      //   password: 'admin123',
-      //   admin: true,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'consultor', //3
-      //   email: 'consultor@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'estoquista', //4
-      //   email: 'mateus@example.com',
-      //   password: 'maria123',
-      //   admin: false,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'crm', //5
-      //   email: 'crm@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'consultor estoquista', //6
-      //   email: 'consultor_estoquista@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'estoquista crm', //7
-      //   email: 'estoquista_crm@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'tudo mas n admin', //8
-      //   email: 'tudo@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: true,
-      // },
-      // {
-      //   fullName: 'desativado 1', //9
-      //   email: 'desativado1@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: false,
-      // },
-      // {
-      //   fullName: 'desativado 2', //10
-      //   email: 'desativado2@example.com',
-      //   password: 'senha123',
-      //   admin: false,
-      //   active: false,
-      // },
-      // {
-      //   fullName: 'admin desativado', //11
-      //   email: 'admin_desativado@example.com',
-      //   password: 'senha123',
-      //   admin: true,
-      //   active: false,
-      // },
+      {
+        fullName: 'admin 2', //2
+        email: 'victor@example.com',
+        password: 'admin123',
+        admin: true,
+        active: true,
+      },
+      {
+        fullName: 'consultor', //3
+        email: 'consultor@example.com',
+        password: 'senha123',
+        admin: false,
+        active: true,
+      },
+      {
+        fullName: 'estoquista', //4
+        email: 'mateus@example.com',
+        password: 'maria123',
+        admin: false,
+        active: true,
+      },
+      {
+        fullName: 'crm', //5
+        email: 'crm@example.com',
+        password: 'senha123',
+        admin: false,
+        active: true,
+      },
+      {
+        fullName: 'consultor estoquista', //6
+        email: 'consultor_estoquista@example.com',
+        password: 'senha123',
+        admin: false,
+        active: true,
+      },
+      {
+        fullName: 'estoquista crm', //7
+        email: 'estoquista_crm@example.com',
+        password: 'senha123',
+        admin: false,
+        active: true,
+      },
+      {
+        fullName: 'tudo mas n admin', //8
+        email: 'tudo@example.com',
+        password: 'senha123',
+        admin: false,
+        active: true,
+      },
+      {
+        fullName: 'desativado 1', //9
+        email: 'desativado1@example.com',
+        password: 'senha123',
+        admin: false,
+        active: false,
+      },
+      {
+        fullName: 'desativado 2', //10
+        email: 'desativado2@example.com',
+        password: 'senha123',
+        admin: false,
+        active: false,
+      },
+      {
+        fullName: 'admin desativado', //11
+        email: 'admin_desativado@example.com',
+        password: 'senha123',
+        admin: true,
+        active: false,
+      },
     ])
   }
 }
